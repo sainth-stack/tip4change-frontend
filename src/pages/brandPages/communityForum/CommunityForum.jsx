e2da3410d8774f334adb154a4520d84b17c61b90
GurusaiKumarredday143 <basamgurusai123@gmail.com>
2025-03-24 23:07:25 +0530

    added  brand page sections

--- src/pages/brandPages/communityForum/CommunityForum.jsx
+++ src/pages/brandPages/communityForum/CommunityForum.jsx
@@ -1,10 +1,11 @@
 import React from 'react'
 import ComingSoon from '../../../components/Page-404/ComingSoon';
+import PostCard from './forum'
 
 const CommunityForum = () => {
   return (
     <>
-      <ComingSoon />
+      <PostCard />
     </>
   );
 }
--- src/pages/brandPages/new&updates/NewsUpdates.jsx
+++ src/pages/brandPages/new&updates/NewsUpdates.jsx
@@ -1,10 +1,11 @@
 import React from 'react'
 import ComingSoon from '../../../components/Page-404/ComingSoon';
+import ArticleList from './cardsSection/section2Cards'
 
 const NewsUpdates = () => {
   return (
     <>
-      <ComingSoon />
+      <ArticleList />
     </>
   );
 }
