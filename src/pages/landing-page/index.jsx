47a38fbe3beb17e336e56e58dd2cb9059ed4b1ee
sainath-stylishop <sainath@stylishop.com>
2025-03-25 23:54:45 +0530

    updated

--- src/pages/landing-page/index.jsx
+++ src/pages/landing-page/index.jsx
@@ -3,10 +3,6 @@ import Section2 from "./sections/section2";
 import Section3 from "./sections/section3";
 import Section4 from "./sections/section4";
 import Section5 from "./sections/section5";
-import DoughnutChartsGrid from '../section3/section3'
-import Table from "../brandPages/social-passport/section2/table";
-import Table1 from "../brandPages/analytics-insights/table/table";
-import Table2 from "../brandPages/consumer-engagement/table/table";
 
 const LandingPage = () => {
   return (
