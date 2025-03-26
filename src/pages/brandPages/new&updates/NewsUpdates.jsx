60862fdf4b8a7773557183efa956b387839cfcd2
VishnuVardhanBonala-web4U <vardhancm08@gmail.com>
2025-03-26 22:17:30 +0530

    updated maps images

--- src/pages/brandPages/new&updates/NewsUpdates.jsx
+++ src/pages/brandPages/new&updates/NewsUpdates.jsx
@@ -1,10 +1,12 @@
 import React from 'react'
 import ComingSoon from '../../../components/Page-404/ComingSoon';
 import ArticleList from './cardsSection/section2Cards'
+import SustainabilityCard from '../../../component/Card/News';
 
 const NewsUpdates = () => {
   return (
     <>
+      <SustainabilityCard />
       <ArticleList />
     </>
   );
--- src/pages/brandPages/social-passport/Section3/index.jsx
+++ src/pages/brandPages/social-passport/Section3/index.jsx
@@ -1,5 +1,5 @@
 import React from "react";
-import { Box } from '@mui/material';
+import { Box, Stack } from '@mui/material';
 import CardWidget from "../../../../components/Card/CardWidget";
 import MultiLineChart from "../../../../components/AllGraphs/MultiLineGraph";
 import { LineChartDataSets, LineChartLabels } from "./data";
@@ -7,7 +7,16 @@ import WorldMapChart from "../../../../components/AllGraphs/WorldMapChart/WorldM
 import { MapsampleData } from "../../SupplyChainSection/Section2/data";
 
 import * as d3 from "d3";
+import CustomTypography from "../../../../components/TypoGraphy/CustomTypography";
+import mapImage from "/src/assets/images/small_map.png";
+import CustomImage from "../../../../components/CustomImage/CustomImage";
 
+
+const legendItems = [
+  { label: "Certified", color: "#6D743F" },
+  { label: "Moderate", color: "#E1AD01" },
+  { label: "High-Risk", color: "#E35D4F" },
+];
 
 const Section3 = () => {
   return (
@@ -18,7 +27,7 @@ const Section3 = () => {
         alignItems: "center",
         flexWrap: "wrap",
         width: "100%",
-        gap:'24px'
+        gap: "24px",
       }}
     >
       <Box
@@ -43,11 +52,74 @@ const Section3 = () => {
       <Box
         sx={{
           flex: 1,
-width:'40%'
+          width: "40%",
         }}
       >
         <CardWidget>
-          <WorldMapChart
+          <Box
+            sx={{
+              display: "flex",
+              flexDirection: "column",
+              justifyContent: "space-between",
+            }}
+          >
+            <CustomTypography
+              sx={{
+                fontWeight: "600",
+                fontSize: "1.5rem",
+                textAlign: "left",
+                marginRight: "2rem",
+              }}
+            >
+              {"Supplier Distribution & Risk Zones"}
+            </CustomTypography>
+
+            <Stack
+              direction="row"
+              spacing={2}
+              alignItems="center"
+              justifyContent={"space-around"}
+              mt={2}
+            >
+              {legendItems.map((item, index) => (
+                <Stack
+                  key={index}
+                  direction="row"
+                  spacing={1}
+                  alignItems="center"
+                >
+                  <Box
+                    sx={{
+                      width: 20,
+                      height: 20,
+                      backgroundColor: item.color,
+                      borderRadius: "50%",
+                    }}
+                  />
+                  <CustomTypography
+                    sx={{
+                      color: "#0E0E0E4D",
+                      fontSize: "11px",
+                    }}
+                  >
+                    {item.label}
+                  </CustomTypography>
+                </Stack>
+              ))}
+            </Stack>
+          </Box>
+          <CustomImage
+            src={mapImage}
+            alt="map image"
+            styles={{
+              width: "100%",
+              maxHeight: "30vh",
+
+              objectFit: "contain",
+              borderRadius: "50%",
+            }}
+          />
+          {/* <WorldMapChart
             width={200}
             height={230}
             title={"Supplier Distribution & Risk Zones"}
@@ -55,7 +127,7 @@ width:'40%'
             colorScale={d3.scaleSequential(d3.interpolateOrRd)}
             tooltipFormatter={(d) => `<b>${d.country}</b>: ${d.value}`}
             onCountryClick={(d) => alert(`Clicked on ${d.country}: ${d.value}`)}
-          />
+          /> */}
         </CardWidget>
       </Box>
     </Box>
